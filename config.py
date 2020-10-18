4b6f82c4e0078ed4229610bb4f474da39f4ba8ab
reejit <70316178+reejit@users.noreply.github.com>
2020-10-18 20:00:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "1312461996:AAG9X7ZQ28TIyozgHwk8KpOVxkvbpHaUPRY"
 PROJECT_NAME = "CEMEX-BOT"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/reejit/CEMEX-BOT"
+GIT_REPO_URL = "https://github.com/pokurt/GitGram"
