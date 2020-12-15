2ffb201b63372d13926f6ca077bf50e219f744fc
reejit <70316178+reejit@users.noreply.github.com>
2020-12-15 20:54:53 +0530

    Update GitGram.py

--- GitGram.py
+++ GitGram.py
@@ -160,17 +160,18 @@ def git_api(groupid):
                 commit_msg = escape(commit['message']).split("\n")[0]
             else:
                 commit_msg = escape(commit['message'])
-            commits_text += f"{commit_msg}\n<a href='{commit['url']}'>{commit['id'][:7]}</a> - {commit['author']['name']} {escape('<')}{commit['author']['email']}{escape('>')}\n\n"
+            commits_text += f"{commit_msg}\nCommit ID:<a href='{commit['url']}'> by {commit['author']['url']}"
+
             if len(commits_text) > 1000:
-                text = f"""✨ <b>{escape(data['repository']['name'])}</b> - New {len(data['commits'])} commits ({escape(data['ref'].split('/')[-1])})
-{commits_text}
+                text = f"""✨   <b>{escape(data['repository']['name'])}</b> - New {len(data['commits'])} commits for<b>{escape(data['repository']['name'])}</b> Updated Content:({escape(data['ref'].split('/')[-1])})
+Extended Description: {commits_text}
 """
                 response = post_tg(groupid, text, "html")
                 commits_text = ""
         if not commits_text:
             return jsonify({"ok": True, "text": "Commits text is none"})
-        text = f"""✨ <b>{escape(data['repository']['name'])}</b> - New {len(data['commits'])} commits ({escape(data['ref'].split('/')[-1])})
-{commits_text}
+        text = f"""✨ <b>{escape(data['repository']['name'])}</b> - New {len(data['commits'])} commits for<b>{escape(data['repository']['name'])}</b> Updated Content:({escape(data['ref'].split('/')[-1])})
+Extended Description: {commits_text}
 """
         if len(data['commits']) > 10:
             text += f"\n\n<i>And {len(data['commits']) - 10} other commits</i>"
